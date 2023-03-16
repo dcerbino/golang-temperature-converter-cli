7fccba4820ca472d90e46001ca00d867736fff8e
David Cerbino <dcerbino@gmail.com>
2023-03-16 13:27:57 -0300

    impemented main

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -17,11 +19,38 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln(errInvalidArguments.Error())
+	}
+
+	originUnit = strings.ToUpper(os.Args[1])
+	var input float64
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
+		switch originUnit {
+		case "C":
+			n, err := fmt.Scanf("%f", &input)
+			if n == 0 || err != nil {
+				log.Fatalln(errReadingInput)
+			}
+			convertToFahrenheit(input)
+		case "F":
+			n, err := fmt.Scanf("%f", &input)
+			if n == 0 || err != nil {
+				log.Fatalln(errReadingInput)
+			}
+			convertToCelsius(input)
+		default:
+			log.Fatalln("invalid Unit: ", originUnit)
+		}
+
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		fmt.Scanln(&shouldConvertAgain)
+
+		shouldConvertAgain = strings.ToUpper(shouldConvertAgain)
 
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
